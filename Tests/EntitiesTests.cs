0f63f83e656b363360e8c4c6b329da37492f024b
Mads Mikkel Rasmussen <mara@aspit.dk>
2022-06-15 11:50:48 +0200

    Testede vejret. Det er OK.

---
+++ Tests/EntitiesTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+using Entities;
+
+namespace Tests
+{
+    public class EntitiesTests
+    {
+        [Fact]
+        public void CorrectInitialization_ContactInformation()
+        {
+
+        }
+    }
+}
